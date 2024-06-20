cb7b9a488aa28965d9d49f94bbb028b0f35f6b1c
NathnaelBalcha <162681707+NathnaelBalcha@users.noreply.github.com>
2024-06-20 13:46:08 +0300

    Add files via upload

---
+++ Java Program/src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nathnanel Balcha
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.drive();
+        Car car = new Car();
+        car.drive();
+    }
+}
